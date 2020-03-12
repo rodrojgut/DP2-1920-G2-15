de239e7af29ab14490250caffc2f1def8784c02d
Antonio Romero Cáceres <antonioromerocaceres@hotmail.com>
2020-03-12 21:19:54 +0100

    Fixed error

--- src/main/java/org/springframework/samples/petclinic/configuration/SecurityConfiguration.java
+++ src/main/java/org/springframework/samples/petclinic/configuration/SecurityConfiguration.java
@@ -39,7 +39,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 				.antMatchers("/admin/**").hasAnyAuthority("admin")
 				.antMatchers("/owners/**").hasAnyAuthority("owner","admin")				
 				.antMatchers("/vets/**").authenticated()
-				.antMatchers("/diseases/**").hasAnyAuthority("veterinarian", "admin")
+				.antMatchers("/diseases/**").hasAnyAuthority("veterinarian")
 				.anyRequest().denyAll()
 				.and()
 				 	.formLogin()
