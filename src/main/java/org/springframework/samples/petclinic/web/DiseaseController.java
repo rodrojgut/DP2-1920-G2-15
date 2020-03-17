d394047a804f25f122b0b988abbcc2e8bd6d0f5c
Antonio Romero Cáceres <antonioromerocaceres@hotmail.com>
2020-03-17 12:33:33 +0100

    Arreglado Create

--- src/main/java/org/springframework/samples/petclinic/web/DiseaseController.java
+++ src/main/java/org/springframework/samples/petclinic/web/DiseaseController.java
@@ -16,9 +16,7 @@
 package org.springframework.samples.petclinic.web;
 
 import java.util.Collection;
-import java.util.Map;
 
-import javax.naming.Binding;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,12 +28,11 @@ import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
+
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import net.bytebuddy.asm.Advice.Return;
 
 import org.springframework.samples.petclinic.service.PetService;
 
@@ -85,7 +82,7 @@ public class DiseaseController {
 		return view;
 	}
 
-	@PostMapping("/save")
+	@PostMapping("/new")
 	public String newDisease(@Valid Disease disease, BindingResult result, ModelMap modelMap){
 		String view = "diseases/diseasesList";
 		if(result.hasErrors()){
