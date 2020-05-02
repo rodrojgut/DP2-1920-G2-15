981c6a250feb3771c4d4f1dddf76a561b76385ce
anton <antonioromerocaceres@hotmail.com>
2020-05-02 17:48:21 +0200

    Arreglado problema de los tests de mySQL con este servicio

--- src/test/java/org/springframework/samples/petclinic/service/BookingServiceTest.java
+++ src/test/java/org/springframework/samples/petclinic/service/BookingServiceTest.java
@@ -10,6 +10,8 @@ import org.assertj.core.api.Assertions;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.samples.petclinic.model.Booking;
@@ -23,6 +25,7 @@ import org.springframework.samples.petclinic.repository.VetRepository;
 import org.springframework.stereotype.Service;
 
 @DataJpaTest(includeFilters = @ComponentScan.Filter(Service.class))
+@AutoConfigureTestDatabase(replace = Replace.NONE)
 class BookingServiceTest {
 
 	@Autowired
