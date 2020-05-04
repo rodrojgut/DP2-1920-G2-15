b4909be3a8bd3e2d7121195c253b6fb8682e21cc
ijnfu <ijnfu@LAPTOP-9CJTD0I6>
2020-05-04 19:36:05 +0200

    Correcciones de errores

--- src/test/java/org/springframework/samples/petclinic/ui/opinion/CreateOpinionUITest.java
+++ src/test/java/org/springframework/samples/petclinic/ui/opinion/CreateOpinionUITest.java
@@ -1,17 +1,19 @@
+
 package org.springframework.samples.petclinic.ui.opinion;
 
-import java.util.regex.Pattern;
 import java.util.concurrent.TimeUnit;
-import org.junit.jupiter.api.*;
+
+import org.junit.Assert;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-
-import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.*;
-import org.openqa.selenium.*;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.Select;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -19,86 +21,88 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class CreateOpinionUITest {
+
 	@LocalServerPort
-    private int port;
-    private WebDriver driver;
-    private String baseUrl;
-    private boolean acceptNextAlert = true;
-    private StringBuffer verificationErrors = new StringBuffer();
+	private int				port;
+	private WebDriver		driver;
+	private String			baseUrl;
+	private boolean			acceptNextAlert		= true;
+	private StringBuffer	verificationErrors	= new StringBuffer();
 
-  @BeforeEach
-  public void setUp() throws Exception {
-	  String pathToChromeDriver = System.getenv("webdriver.chrome.driver");
-	  System.setProperty("webdriver.chrome.driver", pathToChromeDriver);
-      driver = new ChromeDriver();
-      baseUrl = "https://www.google.com/";
-      driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-  }
 
-  @Test
-  public void testCreateOpinionUI() throws Exception {
-    driver.get("http://localhost:8080/");
-    driver.findElement(By.linkText("LOGIN")).click();
-    driver.findElement(By.id("username")).clear();
-    driver.findElement(By.id("username")).sendKeys("admin1");
-    driver.findElement(By.id("password")).click();
-    driver.findElement(By.id("password")).clear();
-    driver.findElement(By.id("password")).sendKeys("4dm1n");
-    driver.findElement(By.xpath("//button[@type='submit']")).click();
-    driver.findElement(By.xpath("//div[@id='main-navbar']/ul/li[3]/a/span[2]")).click();
-    driver.findElement(By.linkText("Add Opinion")).click();
-    driver.findElement(By.id("comentary")).click();
-    driver.findElement(By.id("comentary")).clear();
-    driver.findElement(By.id("comentary")).sendKeys("Es un poco tonto");
-    driver.findElement(By.id("puntuation")).click();
-    driver.findElement(By.id("puntuation")).clear();
-    driver.findElement(By.id("puntuation")).sendKeys("2");
-    driver.findElement(By.xpath("//button[@type='submit']")).click();
-    driver.findElement(By.linkText("List all opinions")).click();
-    assertEquals("James Carter", driver.findElement(By.xpath("//table[@id='opinionsTable']/tbody/tr[7]/td")).getText());
-    assertEquals("2", driver.findElement(By.xpath("//table[@id='opinionsTable']/tbody/tr[7]/td[2]")).getText());
-    assertEquals("Es un poco tonto", driver.findElement(By.xpath("//table[@id='opinionsTable']/tbody/tr[7]/td[4]")).getText());
-  }
+	@BeforeEach
+	public void setUp() throws Exception {
+		String pathToChromeDriver = System.getenv("webdriver.chrome.driver");
+		System.setProperty("webdriver.chrome.driver", pathToChromeDriver);
+		this.driver = new ChromeDriver();
+		this.baseUrl = "https://www.google.com/";
+		this.driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
 
-  @AfterEach
-  public void tearDown() throws Exception {
-    driver.quit();
-    String verificationErrorString = verificationErrors.toString();
-    if (!"".equals(verificationErrorString)) {
-      fail(verificationErrorString);
-    }
-  }
+	@Test
+	public void testCreateOpinionUI() throws Exception {
+		this.driver.get("http://localhost:" + this.port + "/");
+		this.driver.findElement(By.linkText("LOGIN")).click();
+		this.driver.findElement(By.id("username")).clear();
+		this.driver.findElement(By.id("username")).sendKeys("admin1");
+		this.driver.findElement(By.id("password")).click();
+		this.driver.findElement(By.id("password")).clear();
+		this.driver.findElement(By.id("password")).sendKeys("4dm1n");
+		this.driver.findElement(By.xpath("//button[@type='submit']")).click();
+		this.driver.findElement(By.xpath("//div[@id='main-navbar']/ul/li[3]/a/span[2]")).click();
+		this.driver.findElement(By.linkText("Add Opinion")).click();
+		this.driver.findElement(By.id("comentary")).click();
+		this.driver.findElement(By.id("comentary")).clear();
+		this.driver.findElement(By.id("comentary")).sendKeys("Es un poco tonto");
+		this.driver.findElement(By.id("puntuation")).click();
+		this.driver.findElement(By.id("puntuation")).clear();
+		this.driver.findElement(By.id("puntuation")).sendKeys("2");
+		this.driver.findElement(By.xpath("//button[@type='submit']")).click();
+		this.driver.findElement(By.linkText("List all opinions")).click();
+		Assert.assertEquals("James Carter", this.driver.findElement(By.xpath("//table[@id='opinionsTable']/tbody/tr[7]/td")).getText());
+		Assert.assertEquals("2", this.driver.findElement(By.xpath("//table[@id='opinionsTable']/tbody/tr[7]/td[2]")).getText());
+		Assert.assertEquals("Es un poco tonto", this.driver.findElement(By.xpath("//table[@id='opinionsTable']/tbody/tr[7]/td[4]")).getText());
+	}
 
-  private boolean isElementPresent(By by) {
-    try {
-      driver.findElement(by);
-      return true;
-    } catch (NoSuchElementException e) {
-      return false;
-    }
-  }
+	@AfterEach
+	public void tearDown() throws Exception {
+		this.driver.quit();
+		String verificationErrorString = this.verificationErrors.toString();
+		if (!"".equals(verificationErrorString)) {
+			Assert.fail(verificationErrorString);
+		}
+	}
 
-  private boolean isAlertPresent() {
-    try {
-      driver.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
-    }
-  }
+	private boolean isElementPresent(final By by) {
+		try {
+			this.driver.findElement(by);
+			return true;
+		} catch (NoSuchElementException e) {
+			return false;
+		}
+	}
 
-  private String closeAlertAndGetItsText() {
-    try {
-      Alert alert = driver.switchTo().alert();
-      String alertText = alert.getText();
-      if (acceptNextAlert) {
-        alert.accept();
-      } else {
-        alert.dismiss();
-      }
-      return alertText;
-    } finally {
-      acceptNextAlert = true;
-    }
-  }
+	private boolean isAlertPresent() {
+		try {
+			this.driver.switchTo().alert();
+			return true;
+		} catch (NoAlertPresentException e) {
+			return false;
+		}
+	}
+
+	private String closeAlertAndGetItsText() {
+		try {
+			Alert alert = this.driver.switchTo().alert();
+			String alertText = alert.getText();
+			if (this.acceptNextAlert) {
+				alert.accept();
+			} else {
+				alert.dismiss();
+			}
+			return alertText;
+		} finally {
+			this.acceptNextAlert = true;
+		}
+	}
 }
--- src/test/java/org/springframework/samples/petclinic/ui/opinion/UpdateOpinionUITest.java
+++ src/test/java/org/springframework/samples/petclinic/ui/opinion/UpdateOpinionUITest.java
@@ -1,18 +1,19 @@
+
 package org.springframework.samples.petclinic.ui.opinion;
 
-
-import java.util.regex.Pattern;
 import java.util.concurrent.TimeUnit;
-import org.junit.jupiter.api.*;
+
+import org.junit.Assert;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-
-import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.*;
-import org.openqa.selenium.*;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.Select;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -21,81 +22,82 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class UpdateOpinionUITest {
 
-    @LocalServerPort
-    private int port;
-    private WebDriver driver;
-    private String baseUrl;
-    private boolean acceptNextAlert = true;
-    private StringBuffer verificationErrors = new StringBuffer();
+	@LocalServerPort
+	private int				port;
+	private WebDriver		driver;
+	private String			baseUrl;
+	private boolean			acceptNextAlert		= true;
+	private StringBuffer	verificationErrors	= new StringBuffer();
 
-    @BeforeEach
-    public void setUp() throws Exception {
-    	String pathToChromeDriver = System.getenv("webdriver.chrome.driver");
-    	System.setProperty("webdriver.chrome.driver", pathToChromeDriver);
-        driver = new ChromeDriver();
-        baseUrl = "https://www.google.com/";
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-    }
 
-    @Test
-    public void testUpdateOpinionUI() throws Exception {
-        driver.get("http://localhost:" + port + "/");
-        driver.findElement(By.linkText("LOGIN")).click();
-        driver.findElement(By.id("username")).clear();
-        driver.findElement(By.id("username")).sendKeys("vet1");
-        driver.findElement(By.id("password")).clear();
-        driver.findElement(By.id("password")).sendKeys("v3t");
-        driver.findElement(By.xpath("//button[@type='submit']")).click();
-        driver.findElement(By.linkText("//div[@id='main-navbar']/ul/li[3]/a")).click();
-        driver.findElement(By.linkText("List all opinions")).click();
-        driver.findElement(By.linkText("List my opinions")).click();
-        driver.findElement(By.linkText("Edit")).click();
-        driver.findElement(By.id("puntuation")).click();
-        driver.findElement(By.id("puntuation")).clear();
-        driver.findElement(By.id("puntuation")).sendKeys("4");
-        driver.findElement(By.xpath("//button[@type='submit']")).click();
-        assertEquals("4", driver.findElement(By.xpath("//table[@id='opinionsTable']/tbody/tr/td[2]")).getText());
-    }
+	@BeforeEach
+	public void setUp() throws Exception {
+		String pathToChromeDriver = System.getenv("webdriver.chrome.driver");
+		System.setProperty("webdriver.chrome.driver", pathToChromeDriver);
+		this.driver = new ChromeDriver();
+		this.baseUrl = "https://www.google.com/";
+		this.driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
 
-    @AfterEach
-    public void tearDown() throws Exception {
-        driver.quit();
-        String verificationErrorString = verificationErrors.toString();
-        if (!"".equals(verificationErrorString)) {
-            fail(verificationErrorString);
-        }
-    }
+	@Test
+	public void testUpdateOpinionUI() throws Exception {
+		this.driver.get("http://localhost:" + this.port + "/");
+		this.driver.findElement(By.linkText("LOGIN")).click();
+		this.driver.findElement(By.id("username")).clear();
+		this.driver.findElement(By.id("username")).sendKeys("owner1");
+		this.driver.findElement(By.id("password")).clear();
+		this.driver.findElement(By.id("password")).sendKeys("0wn3r");
+		this.driver.findElement(By.xpath("//button[@type='submit']")).click();
+		this.driver.findElement(By.xpath("//div[@id='main-navbar']/ul/li[3]/a/span[2]")).click();
+		this.driver.findElement(By.linkText("List all opinions")).click();
+		this.driver.findElement(By.linkText("List my opinions")).click();
+		this.driver.findElement(By.linkText("Edit")).click();
+		this.driver.findElement(By.id("puntuation")).click();
+		this.driver.findElement(By.id("puntuation")).clear();
+		this.driver.findElement(By.id("puntuation")).sendKeys("4");
+		this.driver.findElement(By.xpath("//button[@type='submit']")).click();
+		Assert.assertEquals("4", this.driver.findElement(By.xpath("//table[@id='opinionsTable']/tbody/tr/td[2]")).getText());
+	}
 
-    private boolean isElementPresent(By by) {
-        try {
-            driver.findElement(by);
-            return true;
-        } catch (NoSuchElementException e) {
-            return false;
-        }
-    }
+	@AfterEach
+	public void tearDown() throws Exception {
+		this.driver.quit();
+		String verificationErrorString = this.verificationErrors.toString();
+		if (!"".equals(verificationErrorString)) {
+			Assert.fail(verificationErrorString);
+		}
+	}
 
-    private boolean isAlertPresent() {
-        try {
-            driver.switchTo().alert();
-            return true;
-        } catch (NoAlertPresentException e) {
-            return false;
-        }
-    }
+	private boolean isElementPresent(final By by) {
+		try {
+			this.driver.findElement(by);
+			return true;
+		} catch (NoSuchElementException e) {
+			return false;
+		}
+	}
 
-    private String closeAlertAndGetItsText() {
-        try {
-            Alert alert = driver.switchTo().alert();
-            String alertText = alert.getText();
-            if (acceptNextAlert) {
-                alert.accept();
-            } else {
-                alert.dismiss();
-            }
-            return alertText;
-        } finally {
-            acceptNextAlert = true;
-        }
-    }
+	private boolean isAlertPresent() {
+		try {
+			this.driver.switchTo().alert();
+			return true;
+		} catch (NoAlertPresentException e) {
+			return false;
+		}
+	}
+
+	private String closeAlertAndGetItsText() {
+		try {
+			Alert alert = this.driver.switchTo().alert();
+			String alertText = alert.getText();
+			if (this.acceptNextAlert) {
+				alert.accept();
+			} else {
+				alert.dismiss();
+			}
+			return alertText;
+		} finally {
+			this.acceptNextAlert = true;
+		}
+	}
 }
