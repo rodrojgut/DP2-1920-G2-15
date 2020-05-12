72bd3cc701e9ec4abebf9c20669c4a79dfce5b30
Thibaut <Michel@DESKTOP-9J3RD28>
2020-05-12 12:41:33 +0200

    Borrado de ficheros sobrantes y anadido @Transacional

--- src/test/java/org/springframework/samples/petclinic/web/endToEnd/PetControllerE2ETest.java
+++ src/test/java/org/springframework/samples/petclinic/web/endToEnd/PetControllerE2ETest.java
@@ -27,11 +27,13 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(
   webEnvironment=SpringBootTest.WebEnvironment.MOCK)
 @AutoConfigureMockMvc
+@Transactional
 public class PetControllerE2ETest {
 	
 	private static final int TEST_OWNER_ID = 1;
